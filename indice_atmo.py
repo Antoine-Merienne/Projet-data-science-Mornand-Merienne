b0ce8349783b14ad7bb772942f90823f13e9c666
Antoine-Merienne <antoine.merienne@ensae.fr>
2024-10-25 09:54:39 +0200

    documentation fonction atmo

---
+++ indice_atmo.py
@@ -0,0 +1,13 @@
+def atmo(mesure_polluants):
+
+    '''Cette fonction calcule l'indice Atmo associé à la mesure de polluants.\n
+    L'argument "mesure_polluants est une liste comprenant 5 valeurs mesures de polluants suivants :\n
+    Abbréviation | Nom | Standard\n
+    PM2.5 |Particules fines < 2.5 microns | Moyenne journalière\n
+    PM10 |Particules fines < 10 microns| Moyenne journalière\n
+    NO2 | Dioxyde d'azote | Maximum horaire journalier\n
+    O3 | Ozone | Maximum horaire journalier\n
+    SO2 | Dioxyde de soufre | Maximum horaire journalier\n
+    '''
+
+    return valeur_indice
