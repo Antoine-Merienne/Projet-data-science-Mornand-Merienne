f92df39b51e54d88172b57e0b5acf3709dfc96ea
Antoine-Merienne <antoine.merienne@ensae.fr>
2024-10-25 11:20:38 +0200

    ajout de la fonction IQA et corrections après tests

--- indice_atmo.py
+++ indice_atmo.py
@@ -9,11 +9,13 @@ def atmo(mesure_polluants):
     NO2 | Dioxyde d'azote | Maximum horaire journalier\n
     O3 | Ozone | Maximum horaire journalier\n
     SO2 | Dioxyde de soufre | Maximum horaire journalier\n
+
+    Remarque : l'argument d'entrée doit correspondre dans l'odre aux mesures des polluants susmentionnés
     '''
 
     valeurs_intermediaires = [0,0,0,0,0]
 
-    #Tableau de référence issue de la législationdes bornes supéreures de chaque catégorie de qualité de l'aire
+    #Tableau de référence issue de la législation des bornes supéreures de chaque catégorie de qualité de l'aire
     #Pour plus d'information consulter https://www.atmo-france.org/sites/federation/files/medias/documents/2022-04/guide_calcul_nouvel_indice_ATMO_VF_version14decembre2020.pdf
     tableau_ref = [
         [10,20,25,50,75],
@@ -25,9 +27,9 @@ def atmo(mesure_polluants):
     
     #On assigne à chaque mesure de qualité de l'air une valeur entre 0 et 4 en fonction de la catégorie associée
     for i in range(len(mesure_polluants)):
-        if mesure_polluants[i] > tableau_ref[4] :
+        if mesure_polluants[i] > tableau_ref[i][4] :
             valeurs_intermediaires[i] = 5
-        elif :
+        else:
             for j in range(4):
                 if tableau_ref[i][j] <= mesure_polluants[i] <= tableau_ref[i][j+1] :
                     valeurs_intermediaires[i] = j
@@ -56,3 +58,23 @@ def atmo(mesure_polluants):
             valeur_indice = "indéterminé"
     
     return valeur_indice
+
+def iqa(mesure_polluants_iqa):
+    
+    '''
+    Cette fonction calcule l'indice IQA associé à la mesure de polluants.\n
+    L'argument "mesure_polluants_iqa est une liste comprenant 5 valeurs mesures de polluants suivants :\n
+    Abbréviation | Nom | Standard\n
+    O3 | Ozone | Moyenne horaire journalière\n
+    PM2.5 |Particules fines < 2.5 microns | Moyenne horaire heures\n
+    SO2 | Dioxyde de soufre | Maximum horaire journalier\n
+    NO2 | Dioxyde d'azote | Moyenne horaire journalier\n
+    CO |Monoxyde de carbone | Moyenne horaire journalière\n
+
+    Remarque : l'argument d'entrée doit correspondre dans l'odre aux mesures des polluants susmentionnés
+    '''
+
+    valeurs_ref = [82,35,200,213,30]
+    valeurs_indice = [50*mesure_polluants_iqa[i]/valeurs_ref[i] for i in range(5)]
+
+    return max(valeurs_indice)
